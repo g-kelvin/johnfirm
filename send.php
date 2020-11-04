a0d47e25f7124e39341698c90485c1fe2b635f08
g-kelvin <kelvingauki@gmail.com>
2020-11-04 22:04:40 +0300

    update

--- send.php
+++ send.php
@@ -1,32 +1,321 @@
-<?php
-if(!isset($_POST['submit']))
-{
-$name = $_POST['name'];
-$email = $_POST['email'];
-$subject = $_POST['subject'];
-$message= $_POST['message'];
+ 
+<!DOCTYPE html>
+<html lang="en">
 
-echo "$name";
-$to = "info@njerunyaga.co.ke";
-$subject = 'Form Submission';
-$msg ="Name:".$name."Tel:".$number."wrote the following:".$message;
-$headers ="From: ".$email;
-echo "msg";
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
-if(mail($to,$subject,$msg,$headers)){
-	echo "sent";
+    <!-- Title -->
+    <title>Njeru Nyaga & Co. Certified Public Accountants</title>
+
+    <!-- Favicon -->
+    <link rel="icon" href="img/core-img/favicon.ico">
+
+    <!-- Stylesheet -->
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+    <!-- Preloader -->
+    <div class="preloader d-flex align-items-center justify-content-center">
+        <div class="lds-ellipsis">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>
+
+    <!-- ##### Header Area Start ##### -->
+    <header class="header-area">
+        <!-- Top Header Area -->
+        <div class="top-header-area">
+            <div class="container h-100">
+                <div class="row h-100 align-items-center">
+                    <div class="col-12 d-flex justify-content-between">
+                        <!-- Logo Area -->
+                         <div class="logo">
+                            <!-- <a href="index.html" style="color: #0000A0" >  <STRONG >Njeru Nyaga & Company</STRONG>  <BR><SMALL  >&nbsp;&nbsp;&nbsp;Certified Public Accountants</SMALL>  </a> -->
+                            <a href="index.html"><img src="img/core-img/one.png" alt=""></a>
+                        </div>
+
+                        <!-- Top Contact Info -->
+                        <div class="top-contact-info d-flex align-items-center">
+                            <a href="#" data-toggle="tooltip" data-placement="bottom" title="Block H, Showbe Plaza, 2nd Flr, Murang'a Rd /Forest Rd ,Nairobi"><img src="img/core-img/placeholder.png" alt=""> <span>Block H, Showbe Plaza, 2nd Flr, Murang'a Rd /Forest Rd ,Nairobi</span></a>
+                            <a href="#" data-toggle="tooltip" data-placement="bottom" title="info@njerunyaga.co.ke"><img src="img/core-img/message.png" alt=""> <span>info@njerunyaga.co.ke</span></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Navbar Area -->
+        <div class="credit-main-menu" id="sticker">
+            <div class="classy-nav-container breakpoint-off">
+                <div class="container">
+                    <!-- Menu -->
+                    <nav class="classy-navbar justify-content-between" id="creditNav">
+
+                        <!-- Navbar Toggler -->
+                        <div class="classy-navbar-toggler">
+                            <span class="navbarToggler"><span></span><span></span><span></span></span>
+                        </div>
+
+                        <!-- Menu -->
+                        <div class="classy-menu">
+
+                            <!-- Close Button -->
+                            <div class="classycloseIcon">
+                                <div class="cross-wrap"><span class="top"></span><span class="bottom"></span></div>
+                            </div>
+
+                            <!-- Nav Start -->
+                            <div class="classynav">
+                                <ul>
+                                    <li><a href="index.html">Home</a></li>
+                                    <li><a href="about.html">About Us</a></li>
+                                    
+                                    <li><a href="services.html">Services</a></li>
+                                    <li><a href="#">What we Offer</a>
+                                        <div class="megamenu">
+                                            <ul class="single-mega cn-col-4">
+                                                <li><a href="audit.html">Auditing Services</a></li>
+                                                <li><a href="tax.html">Tax Services </a></li>
+                                               
+                                            </ul>
+                                            <ul class="single-mega cn-col-4">
+                                                <li><a href="accounting.html">Accounting Services</a></li>
+                                                <li><a href="payroll.html">Payroll Services</a></li>
+                                                
+                                            </ul>
+                                            <ul class="single-mega cn-col-4">
+                                                <li><a href="secretarial.html">Secretarial Services</a></li>
+                                                
+                                            </ul>
+                                        </div>
+                                    </li>
+                                   
+                                    <li><a href="contact.html">Contact</a></li>
+                                </ul>
+                            </div>
+                            <!-- Nav End -->
+                        </div>
+
+                        <!-- Contact -->
+                        <div class="contact">
+                            <a href="#"><img src="img/core-img/call2.png" alt=""> +254 722 707233</a>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- ##### Header Area End ##### -->
+
+    <!-- ##### Breadcrumb Area Start ##### -->
+    <section class="breadcrumb-area bg-img bg-overlay jarallax" style="background-image: url(img/bg-img/13.jpg);">
+        <div class="container h-100">
+            <div class="row h-100 align-items-center">
+                <div class="col-12">
+                    <div class="breadcrumb-content">
+                        <h2>MESSAGES</h2>
+                        <nav aria-label="breadcrumb">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                                <li class="breadcrumb-item active" aria-current="page">Messaging</li>
+                            </ol>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- ##### Breadcrumb Area End ##### -->
+
+    
+<?php 
+    if(!isset($_POST['submit'])){
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $subject = $_POST['subject'];
+        $message = $_POST['message'];
+
+        
+       
+        $to = "info@njerunyaga.co.ke";
+        $subject ="Email from Website. Subjects is: ".$subject;
+        $message ="My Name is : ".$name. " of Email: ".$email." The Following is my message: ".$message;
+        $headers ="From: ".$email;
+         if(mail($to,$subject,$message,$headers)){
+            
+          echo " <b>Message Sent. Thank You $name  for your Message.<b>";
 }
+
 else {
-	echo "yolo";
+    echo "Not Sent";
+    
 }
 
-
-}
-else{
-	echo "error on Submission";
-}
+       
 
 
+    }
+    else
+    {
+        echo "try again";
+    }
+
+ ?>
+
+    
+    
+
+      <!-- ##### Footer Area Start ##### -->
+    <footer class="footer-area section-padding-100-0">
+        <div class="container">
+            <div class="row">
+
+                <!-- Single Footer Widget -->
+                <div class="col-12 col-sm-6 col-lg-3">
+                    <div class="single-footer-widget mb-100">
+                        <h5 class="widget-title">About Us</h5>
+                        <!-- Nav -->
+                         <nav>
+                        
+                                <p style="color: white">Njeru Nyaga and Co is a medium size firm of Certified Public Accountants which was founded in 1989.It is located in Pangani –Showbe Plaza in Muranga Road. The firm was founded by a team of highly qualified and..... <a href="about.html" style="color: blue">Read More</a> </p>
+                                
+                           
+                        </nav>
+                    </div>
+                </div>
+
+                <!-- Single Footer Widget -->
+                <div class="col-12 col-sm-6 col-lg-3">
+                    <div class="single-footer-widget mb-100">
+                        <h5 class="widget-title">Quick Links</h5>
+                        <!-- Nav -->
+                        <nav>
+                            <ul>
+                                <li><a href="index.html">Home</a></li>
+                                <li><a href="about.html">About Us</a></li>
+                                <li><a href="services.html">Services</a></li>
+                                <li><a href="contact.html">Contact Us</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+
+                <!-- Single Footer Widget -->
+                <div class="col-12 col-sm-6 col-lg-3">
+                    <div class="single-footer-widget mb-100">
+                        <h5 class="widget-title">Our Address</h5>
+                        <!-- Nav -->
+                        <nav>
+                            <ul style="color: white">
+                            <li><i class="flaticon-032-placeholder"></i>P.O Box 1234, Block H, Showbe Plaza, 2nd Flr, Murang'a Rd /Forest Rd ,Nairobi - Kenya</li>
+                             <li><i class="flaticon-025-arroba"></i>info@njerunyaga.com</li>
+                              <p style="color: white">+254 722 707233</p>
+                            <li><i class="flaticon-038-wall-clock"></i>Everyday: 08:00 -18:00 pm</li>
+                        </ul>
+                        </nav>
+                    </div>
+                </div>
+
+                <!-- Single Footer Widget -->
+                <div class="col-12 col-sm-6 col-lg-3">
+                    <div class="single-footer-widget mb-100">
+                        <h5 class="widget-title">Latest News</h5>
+
+                        <!-- Single News Area -->
+                        <div class="single-latest-news-area d-flex align-items-center">
+                            <div class="news-thumbnail">
+                                <img src="img/bg-img/7.jpg" alt="">
+                            </div>
+                            <div class="news-content">
+                                <a href="#">How to Manage your Tax</a>
+                                <div class="news-meta">
+                                    <a href="#" class="post-author"><img src="img/core-img/pencil.png" alt=""> Jane Smith</a>
+                                    <a href="#" class="post-date"><img src="img/core-img/calendar.png" alt=""> April 26</a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Single News Area -->
+                        <div class="single-latest-news-area d-flex align-items-center">
+                            <div class="news-thumbnail">
+                                <img src="img/bg-img/8.jpg" alt="">
+                            </div>
+                            <div class="news-content">
+                                <a href="#">A new way to get a loan</a>
+                                <div class="news-meta">
+                                    <a href="#" class="post-author"><img src="img/core-img/pencil.png" alt=""> Jane Smith</a>
+                                    <a href="#" class="post-date"><img src="img/core-img/calendar.png" alt=""> April 26</a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Single News Area -->
+                        <div class="single-latest-news-area d-flex align-items-center">
+                            <div class="news-thumbnail">
+                                <img src="img/bg-img/9.jpg" alt="">
+                            </div>
+                            <div class="news-content">
+                                <a href="#">Finance Your Business</a>
+                                <div class="news-meta">
+                                    <a href="#" class="post-author"><img src="img/core-img/pencil.png" alt=""> Jane Smith</a>
+                                    <a href="#" class="post-date"><img src="img/core-img/calendar.png" alt=""> April 26</a>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Copywrite Area -->
+        <div class="copywrite-area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="copywrite-content d-flex flex-wrap justify-content-between align-items-center">
+                            <!-- Footer Logo -->
+                            <a href="index.html"  > <h4 style="color: white"> <STRONG >Njeru Nyaga & Company</STRONG>  <BR><SMALL  >&nbsp;&nbsp;&nbsp;Certified Public Accountants</SMALL>  </h4></a>
+
+                            <!-- Copywrite Text -->
+                            <p class="copywrite-text"><a href="#"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved |Developed </i> By <a href="https://softwaretechn.co.ke" target="_blank"><b style="color: white">Software Technology Kenya</b></a>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- ##### Footer Area Start ##### -->
+
+    <!-- ##### All Javascript Script ##### -->
+    <!-- jQuery-2.2.4 js -->
+    <script src="js/jquery/jquery-2.2.4.min.js"></script>
+    <!-- Popper js -->
+    <script src="js/bootstrap/popper.min.js"></script>
+    <!-- Bootstrap js -->
+    <script src="js/bootstrap/bootstrap.min.js"></script>
+    <!-- All Plugins js -->
+    <script src="js/plugins/plugins.js"></script>
+    <!-- Active js -->
+    <script src="js/active.js"></script>
+</body>
+
+</html>
 
 
-?> 
+
+
+
+ 
